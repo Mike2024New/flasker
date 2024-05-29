6a54badf63d09a8c2c12e3a73c9687d1b1ffae23
Mike2024New <ProggerMan2022@yandex.ru>
2024-05-29 21:47:56 +0300

    валидация форм wtf первое знакомство

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+# так делать запрещено! ключ должен быть скрыт, например в перемен текущ терминальн сессии
+# но для упрощения учебного примера здесь сделан простой ключ, но в реальном проекте он сложный и скрыт
+app.config['SECRET_KEY'] = "test###"
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+class NameForm(FlaskForm):
+    """в этом классе создаются формы и задаётся валидация этих форм
+    они будут проброшены в flask для дальнейшей работы с ними"""
+    name = StringField("Как Вас зовут?", validators=[DataRequired()])  # здесь будет выполн проверка на пустоту
+    password = PasswordField("Ваш пароль", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    """обработка введенных данных в форму"""
+    name_field = None
+    psw_field = None
+    form = NameForm()
+    if form.validate_on_submit():  # если форма прошла валидацию, то записать переменные и показать приветствие
+        name_field = form.name.data
+        psw_field = form.password.data
+        form.name.data = ''
+    return render_template('name.html', name=name_field, psw=psw_field, form=form)
 
 
 @app.errorhandler(404)
